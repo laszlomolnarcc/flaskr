73e50c7305b87713e5d3aa20663540ee6818d6bd
laszlo.molnar <laszlo.molnar@codecool.com>
2017-02-02 15:52:25 +0100

    Running from flaskr.py removed because it failed because of the import directory which is needed for the ./run.sh

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -78,11 +78,3 @@ def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
-
-# with app.app_context():
-#    init_db()
-#    print(current_app.name + ' started')
-
-
-# if __name__ == "__main__":
-#    app.run(debug=True)
